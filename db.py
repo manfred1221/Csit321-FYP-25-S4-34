4ef7d9db3bfd283d4a9ce24a30e83e9b0bb2620d
manfred1221 <manfred12717@gmail.com>
2025-12-12 13:39:08 +0800

    .

--- db.py
+++ db.py
@@ -3,20 +3,7 @@
 import os
 import psycopg2
 
+# db.py
+# Compatibility wrapper so old code that imports from `db` still works.
 
-def get_db_connection():
-
-    dbname = os.getenv("DB_NAME", "CSIT321")
-    user = os.getenv("DB_USER", "postgres")
-    password = os.getenv("DB_PASSWORD", "joshua1102")
-    host = os.getenv("DB_HOST", "localhost")
-    port = os.getenv("DB_PORT", "5432")
-
-    conn = psycopg2.connect(
-        dbname=dbname,
-        user=user,
-        password=password,
-        host=host,
-        port=port,
-    )
-    return conn
+from database import get_db_connection, get_db_cursor, test_connection
