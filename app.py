130b035834980c349be17765ed228c0dedf75e98
joshuaelijahwoon <joshuaelijahwoon@gmail.com>
2026-02-11 10:11:59 +0800

    Testing

--- app.py
+++ app.py
@@ -1060,8 +1060,9 @@ if SECURITY_OFFICER_AVAILABLE:
             new_embedding = FaceEmbedding(
                 user_type="visitor",
                 reference_id=visitor_id,
-                embedding=embedding_vector.to_list(),
+                embedding=embedding_vector,
             )
+            
             db.session.add(new_embedding)
             db.session.commit()
 
